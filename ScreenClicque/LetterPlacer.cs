5489c687d53cfb908dc3830b8cd73fe7fe5fb4d3
Lev Shumilov <schtinguerch@gmail.com>
2023-01-04 21:54:43 +0500

    Add: Logic for key combination hints placement

--- ScreenClicque/LetterPlacer.cs
+++ ScreenClicque/LetterPlacer.cs
@@ -23,5 +23,92 @@ public class LetterPlacer
     private string _rawLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private string _specialLetters = "1234567890;/\"";
 
+    public LetterPlacer(Window window)
+    {
+        TargetWindow = window;
+        window.KeyDown += WindowOnKeyDown;
+    }
+
+    public void StartSelection(List<Point> locations, List<LetterControl> letters)
+    {
+        _locations = locations;
+        _letters = letters;
+        _pressedShortCut = string.Empty;
+
+        DeclareCombinations();
+
+        for (var i = 0; i < locations.Count; i += 1)
+        {
+            Canvas.SetLeft(letters[i], locations[i].X);
+            Canvas.SetTop(letters[i], locations[i].Y);
+        }
+        
+        TargetWindow.Show();
+        TargetWindow.Activate();
+    }
+
+    private void DeclareCombinations()
+    {
+        _maxShortcutLength = 2;
+        var count = 0;
+        var isLongList = _locations.Count > _rawLetters.Length * _rawLetters.Length;
+
+        for (var i = 0; i < _rawLetters.Length; i += 1)
+        for (var j = 0; j < _rawLetters.Length; j += 1)
+        {
+            if (count >= _locations.Count)
+            {
+                //I know, we all hate the goto, but I'm lazy and know what I do
+                goto lettersMarked;
+            }
+
+            if (isLongList && (i is > 8 and < 14))
+            {
+                continue;
+            }
+
+            count += 1;
+            _letters[count - 1].LettersCharacter = $"{_rawLetters[i]}{_rawLetters[j]}";
+        }
+        
+
+        if (count < _locations.Count)
+        {
+            _maxShortcutLength = 3;
+
+            foreach (var letter in "JKLMN")
+            {
+                for (var j = 0; j < _rawLetters.Length; j += 1)
+                for (var k = 0; k < _rawLetters.Length; k += 1)
+                {
+                    if (count >= _locations.Count)
+                    {
+                        goto lettersMarked;
+                    }
+                
+                    count += 1;
+                    _letters[count - 1].LettersCharacter = $"{letter}{_rawLetters[j]}{_rawLetters[k]}";
+                }
+            }
+        }
+
+        lettersMarked:
+        var taskbarCount = 0;
+
+        for (var i = 0; i < _locations.Count; i += 1)
+        {
+            if (taskbarCount >= _specialLetters.Length)
+            {
+                break;
+            }
+
+            if ((_locations[i].Y > 1030) && (_locations[i].X is > 600 and < 1400))
+            {
+                taskbarCount += 1;
+                _letters[i].LettersCharacter = $"{_specialLetters[taskbarCount - 1]}";
+            }
+        }
+    }
+
     
 }
