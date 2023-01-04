7a2bf3e7620ac5bfb95ec6b318c7edee0ef64aee
Lev Shumilov <schtinguerch@gmail.com>
2023-01-04 21:57:43 +0500

    Add: image thresholding

--- ScreenClicque/GuiRecognizer.cs
+++ ScreenClicque/GuiRecognizer.cs
@@ -13,6 +13,30 @@ namespace ScreenClicque;
 public class GuiRecognizer
 {
     
+
+    private Image<Gray, byte> ProcessedImage(Image<Bgr, byte> sourceImage, bool tryCompress = false, bool tryBlur = false)
+    {
+        var grayscaledImage = sourceImage.Convert<Gray, byte>();
+
+        if (tryCompress)
+        {
+            CvInvoke.Resize(grayscaledImage, grayscaledImage, new System.Drawing.Size(960, 540));
+            CvInvoke.Resize(grayscaledImage, grayscaledImage, new System.Drawing.Size(1920, 1080));
+        }
+
+        if (tryBlur)
+        {
+            CvInvoke.GaussianBlur(grayscaledImage, grayscaledImage, new System.Drawing.Size(3, 3), 3, 3);
+        }
+
+        var thresholdedImage = grayscaledImage.ThresholdAdaptive(
+            new Gray(255),
+            AdaptiveThresholdType.MeanC,
+            ThresholdType.Binary,
+            5, new Gray(1));
+
+        return thresholdedImage;
+    }
     
     private Image<Bgr, byte> GetScreenshot()
     {
