c8c3308c2a4648f7d85c03b506ba1d17c94f9c35
Lev Shumilov <schtinguerch@gmail.com>
2023-01-04 21:58:22 +0500

    Add: Contours analyzing (area, size, position)

--- ScreenClicque/GuiRecognizer.cs
+++ ScreenClicque/GuiRecognizer.cs
@@ -14,6 +14,50 @@ public class GuiRecognizer
 {
     
 
+    //Send "Hello" to the Uncle Bob
+    private (bool, Rectangle) AnalyzeContour(VectorOfPoint contour, ref Point previousCenter)
+    {
+        var area = CvInvoke.ContourArea(contour);
+        var notSuitableOutput = (false, new Rectangle(0, 0, 0, 0));
+
+        if (area < 80 || area > 200_000)
+        {
+            return notSuitableOutput;
+        }
+
+        var rectangleBounds = CvInvoke.BoundingRectangle(contour);
+        var widthRatio = (double) rectangleBounds.Width / rectangleBounds.Height;
+        
+        if (rectangleBounds.Height < 7 || rectangleBounds.Width < 7 || widthRatio > 18)
+        {
+            return notSuitableOutput;
+        }
+
+        var center = rectangleBounds.Center();
+        if (center.Range(previousCenter) < 18)
+        {
+            return notSuitableOutput;
+        }
+
+        previousCenter = center;
+        return (true, rectangleBounds);
+    }
+
+    private (VectorOfVectorOfPoint, Mat) FindContours(Image<Gray, byte> image)
+    {
+        var contours = new VectorOfVectorOfPoint();
+        var hierarchy = new Mat();
+
+        CvInvoke.FindContours(
+            image,
+            contours,
+            hierarchy,
+            RetrType.Tree,
+            ChainApproxMethod.ChainApproxNone);
+
+        return (contours, hierarchy);
+    }
+
     private Image<Gray, byte> ProcessedImage(Image<Bgr, byte> sourceImage, bool tryCompress = false, bool tryBlur = false)
     {
         var grayscaledImage = sourceImage.Convert<Gray, byte>();
