bbc21374493d00cf064c903bfab2acf50e6465c1
Lev Shumilov <schtinguerch@gmail.com>
2023-01-04 21:34:57 +0500

    Add: app startup behavior: loading click menu and click controller

---
+++ ScreenClicque/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ScreenClicque
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var clickMenu = new ClicqueMenu();
+            clickMenu.Show();
+
+            var controller = new HotkeyController(clickMenu);
+            clickMenu.Hide();
+        }
+    }
+}
