ef8ad28c684c9f8d3ac8436725edd4036e1ba317
Lev Shumilov <schtinguerch@gmail.com>
2023-01-04 21:32:29 +0500

    Add: Auto-click functionality

--- ScreenClicque/Mouser.cs
+++ ScreenClicque/Mouser.cs
@@ -31,5 +31,59 @@ public class Mouser
     [DllImport("user32.dll")]
     private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
 
-    
+    public static void SetCursorPosition(int x, int y)
+    {
+        SetCursorPos(x, y);
+    }
+
+    public static void SetCursorPosition(MousePoint point)
+    {
+        SetCursorPos(point.X, point.Y);
+    }
+
+    public static MousePoint GetCursorPosition()
+    {
+        MousePoint currentMousePoint;
+        var gotPoint = GetCursorPos(out currentMousePoint);
+        if (!gotPoint) { currentMousePoint = new MousePoint(0, 0); }
+        return currentMousePoint;
+    }
+
+    public static void MouseEvent(MouseEventFlags value)
+    {
+        MousePoint position = GetCursorPosition();
+
+        mouse_event
+            ((int)value,
+                position.X,
+                position.Y,
+                0,
+                0)
+            ;
+    }
+
+    public static void Click()
+    {
+        MouseEvent(MouseEventFlags.LeftDown);
+        MouseEvent(MouseEventFlags.LeftUp);
+    }
+
+    public static void ClickAt(Point position)
+    {
+        SetCursorPosition(new MousePoint(position.X, position.Y));
+        Click();
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MousePoint
+    {
+        public int X;
+        public int Y;
+
+        public MousePoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
 }
