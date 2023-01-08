0f2b46d2bb1518ec14515e35bd3abca2b66fc79b
Lev Shumilov <schtinguerch@gmail.com>
2023-01-08 21:34:08 +0500

    Add: global try-catch to get info of unhanled exception

--- ScreenClicque/App.xaml.cs
+++ ScreenClicque/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace ScreenClicque
 {
@@ -15,11 +16,21 @@ namespace ScreenClicque
     {
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+            
             var clickMenu = new ClicqueMenu();
             clickMenu.Show();
 
             var controller = new HotkeyController(clickMenu);
             clickMenu.Hide();
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) 
+        {
+            var errorMessage = $"Exception: {e.Exception.Message}";
+            MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            e.Handled = true;
         }
     }
 }
