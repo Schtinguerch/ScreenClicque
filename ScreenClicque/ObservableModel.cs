0319b10c526617b2c82c625db6da7d79b5b9c248
Lev Shumilov <schtinguerch@gmail.com>
2023-01-08 21:31:18 +0500

    Add: MVVM model base

---
+++ ScreenClicque/ObservableModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ScreenClicque;
+
+public class ObservableModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
