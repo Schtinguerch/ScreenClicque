6f1e344f9a297b1abddf7df3fa3631ddf8b43098
Lev Shumilov <schtinguerch@gmail.com>
2023-01-08 21:33:32 +0500

    Add: global entry for recognition settings

---
+++ ScreenClicque/CommonIsland.cs
@@ -0,0 +1,7 @@
+namespace ScreenClicque;
+
+public static class CommonIsland
+{
+    public static RecognizerSettingsModel RecognizerSettings { get; set; } = new RecognizerSettingsModel();
+    public static ImageProcessingSettingsModel ImageProcessingSettings { get; set; } = new ImageProcessingSettingsModel();
+}
